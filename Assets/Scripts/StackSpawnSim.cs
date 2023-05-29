c22b3cb41c849dd1c396f40a461fc28af53bb7bc
Shivansh Pachnanda <114482037+KahnSlaver@users.noreply.github.com>
2023-05-29 14:21:39 +0530

    Final Commit

---
+++ Assets/Scripts/StackSpawnSim.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StackSpawnSim : MonoBehaviour
+{   
+    private int rings;
+    [SerializeField]private GameObject ringPrefab;
+
+    private Vector3 pos;
+    [SerializeField]private Transform stackA;
+    private void Start()
+    {   
+        rings = StackSpawn.ringsCount;
+        pos = new Vector3(stackA.position.x,stackA.position.y+3,stackA.position.z);
+        InvokeRepeating(nameof(spawnRings),0,.2f);
+    }
+
+    private void spawnRings()
+    {
+        if(rings >0)
+        {   
+            GameObject inst = Instantiate(ringPrefab,pos,Quaternion.identity,stackA);
+            inst.transform.SetSiblingIndex(0);
+            inst.name = rings+" Ring";
+            inst.transform.localScale=new Vector3(rings*0.2f+0.5f,ringPrefab.transform.localScale.y,rings*0.2f+0.5f);
+            rings--;
+        }
+        else
+        {
+            TransferRing.game=true;
+        }
+
+    }
+}
